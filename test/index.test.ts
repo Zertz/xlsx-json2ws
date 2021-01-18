bfb21f97564d3ba19123ca43f9e1d5332aab659e
Pier-Luc Gendreau <pierluc@outlook.com>
2021-01-17 20:26:58 -0500

    Update index.test.ts

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,4 @@
+import * as xlsx from "xlsx";
 import { json2xlsx } from "../src";
 
 describe("json2xlsx", () => {
@@ -7,5 +8,23 @@ describe("json2xlsx", () => {
 
   it("returns something", () => {
     expect(json2xlsx([{ bonjour: "hi" }])).toBeTruthy();
+  });
+
+  it.skip("e2e", () => {
+    const Sheet1 = json2xlsx([{ bonjour: "hi" }]);
+
+    if (!Sheet1) {
+      throw Error();
+    }
+
+    xlsx.writeFile(
+      {
+        SheetNames: ["Sheet1", "Sheet2"],
+        Sheets: {
+          Sheet1,
+        },
+      },
+      "workbook.xlsx",
+    );
   });
 });
